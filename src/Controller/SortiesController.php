951ea9b16976697e876f41cb69fcd4764931cbd3
Dinobac <dinobac@free.fr>
2021-08-03 14:33:17 +0200

    création sorties.

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\CreationSortieType;
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortiesController extends AbstractController
 {
-    #[Route('/Sorties', name: 'app_Sorties')]
+    #[Route('/sorties', name: 'app_sorties')]
 
     public function sorties()
     {
         return $this->render('sorties/sorties.html.twig');
 
+    }
+    #[Route('/create', name: 'app_creation')]
+
+    public function create(Request $request): \Symfony\Component\HttpFoundation\Response {
+        $sortie = new Sortie();
+        $creationSortieForm = $this->createForm(CreationSortieType::class, $sortie);
+
+        return $this->render('creation/create.html.twig', [
+            'creationSortieForm' => $creationSortieForm->createView()
+        ]);
     }
 
 }
